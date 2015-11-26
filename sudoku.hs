a0315b832a48b36281e9cbbb125faaad4366c627
Mark Karavan <mark.karavan@gmail.com>
2015-11-26 12:06:11 -0500

    adds consolidate

--- sudoku.hs
+++ sudoku.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Square = Answer Int | Possible [Int] | Suggest Int deriving (Read, Show, Eq)
 type Board = [[Square]]
 
@@ -30,19 +32,39 @@ generateBoard rawStrs = map (\ x -> createRow x) rawStrs
                  then Possible [1..9]
                  else Answer n
 
-solveLoop :: Board -> (Bool, Board)
-solveLoop = undefined
+--solveLoop :: Board -> (Bool, Board)
+--solveLoop board = (True, solveRow board)
 -- these are part of solveLoop
+--solveLoop :: Board -> IO ()
+solveLoop = solveReg $ solveCol $ solveRow $ generateBoard example
+
 solveRow :: Board -> Board
-solveRow = undefined
+solveRow board = map consolidate board
 
 solveCol :: Board -> Board
-solveCol = undefined
+solveCol board = transpose $ map consolidate $ transpose board
 
 solveReg :: Board -> Board
-solveReg = undefined
----------------------
+solveReg board = board
 
+consolidate :: [Square] -> [Square]
+consolidate row =
+    let convertedRow = map convert row
+        answers = getAnswers convertedRow
+    in  map (prune answers) convertedRow
+    where
+          convert :: Square -> Square
+          convert (Possible [x]) = (Answer x)
+          convert x = x
+          getAnswers :: [Square] -> [Int]
+          getAnswers [(Answer x)] = [x]
+          getAnswers [(Possible x)] = []
+          getAnswers ((Answer x):xs) = x : getAnswers xs
+          getAnswers ((Possible x):xs) = getAnswers xs
+          prune :: [Int] -> Square -> Square
+          prune answers (Possible xs) = (Possible (filter (\ x -> not (x `elem` answers)) xs))
+          prune answers (Answer x) = (Answer x)
+---------------------
 
 checkForWin :: Board -> Bool
 checkForWin board = all isAnswer $ concat board
@@ -57,5 +79,10 @@ generateSuggestionList = undefined
 displaySolution :: Board -> IO ()
 displaySolution = undefined
 
+{-
+data Square = Answer Int | Possible [Int] | Suggest Int deriving (Read, Show, Eq)
+type Board = [[Square]]
 
-
+myRow :: [Square]
+myRow = [Answer 5, Answer 1, Possible [4], Answer 7, Answer 2, Possible [1,2,3,4,5,6,7,8,9], Answer 4, Possible [1,2,3,4,5,6,7,8,9], Answer 9]
+-}
