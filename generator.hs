865d4a63131eb330228186c94eef79afa084ee11
Mark Karavan <mark.karavan@gmail.com>
2016-01-08 20:57:00 -0500

    yay it works I think

--- generator.hs
+++ generator.hs
@@ -1,12 +1,24 @@
--- board generator
-
 -- QUESTION: rowswap/colswap take 2 parameters, which each seem to be random integers 1-9. but swapping rows 3 and 4, for example, can create an unsolvable puzzle. how do we know that the swap functions are only operating on pairs 1-3, 4-6, or 7-9?
+
+module Generator (
+  seed, 
+  charSwap,
+  colSwap,
+  rowSwap,
+  swapLoop,
+  diagonalSwap,
+  makeRandomBoard,
+  genBoardLoop,
+  randomParams,
+  randomFunctions
+  ) where
 
 import Data.List
 import Data.Char(intToDigit)
 import System.Random
 
 type Sudoku = [String]
+type Gibberish = Int
 
 seed :: Sudoku
 seed = ["090014070",
@@ -27,13 +39,13 @@ charSwap (x:xs) a b =
        b' = intToDigit b
    in (map (\ c -> if c == a' then b' else if c == b' then a' else c) x) : charSwap xs a b
 
-colSwap :: Sudoku -> Int -> Int -> Sudoku
-colSwap board a b =
-	let tBoard = transpose board
-	in transpose $ swapLoop tBoard a b (tBoard !! a) (tBoard !! b)
+colSwap :: Sudoku -> (Int, Int) -> Sudoku
+colSwap board (a, b) =
+   let tBoard = transpose board
+   in transpose $ swapLoop tBoard a b (tBoard !! a) (tBoard !! b)
 
-rowSwap :: Sudoku -> Int -> Int -> Sudoku
-rowSwap board a b = swapLoop board a b (board !! a) (board !! b)
+rowSwap :: Sudoku -> (Int, Int) -> Sudoku
+rowSwap board (a, b) = swapLoop board a b (board !! a) (board !! b)
 
 swapLoop :: Sudoku -> Int -> Int -> String -> String -> Sudoku
 swapLoop [] _ _ _ _ = []
@@ -50,21 +62,33 @@ diagonalSwap xs = foldl1 (zipWith (++)) $ map rowToCol xs
         rowToCol [] = []
         rowToCol (x:xs) = [x]:(rowToCol xs)
 
-generateBoard' :: Sudoku -> Int -> Sudoku
-generateBoard' seed n = genBoardLoop seed n (randomFunctions n 374657826458726) (randomParams (2 * n) 7829482375982)
+makeRandomBoard :: Sudoku -> Int -> Gibberish -> Sudoku
+makeRandomBoard seed n gibberish = genBoardLoop seed n (randomFunctions n gibberish) (randomParams (2 * n) gibberish)
 
 genBoardLoop :: Sudoku -> Int -> [Int] -> [Int] -> Sudoku
 genBoardLoop board n _ [] = board
 genBoardLoop board n [] _ = board
 genBoardLoop board n (f:fs) (p:p':ps)
-   | f == 1    = genBoardLoop (colSwap board (p - 1) (p' - 1)) n fs ps 
-   | f == 2    = genBoardLoop (rowSwap board (p - 1) (p' - 1)) n fs ps 
+   | f == 1    = genBoardLoop (colSwap board (getSwapPair p)) n fs ps 
+   | f == 2    = genBoardLoop (rowSwap board (getSwapPair p)) n fs ps 
    | f == 3    = genBoardLoop (charSwap board p p') n fs ps 
    | otherwise = genBoardLoop (diagonalSwap board) n fs ps 
 
-randomParams :: Int -> Int -> [Int]
+getSwapPair :: Int -> (Int, Int)
+getSwapPair n = case n of
+   1 -> (0,1)
+   2 -> (0,2)
+   3 -> (1,2)
+   4 -> (3,4)
+   5 -> (3,5)
+   6 -> (4,5)
+   7 -> (6,7)
+   8 -> (6,8)
+   9 -> (7,8)
+
+randomParams :: Int -> Gibberish -> [Int]
 randomParams n seed' = take n . randomRs (1, 9) . mkStdGen $ seed'
 
-randomFunctions :: Int -> Int -> [Int]
+randomFunctions :: Int -> Gibberish -> [Int]
 randomFunctions n seed' = take n . randomRs (1, 4) . mkStdGen $ seed'
 
